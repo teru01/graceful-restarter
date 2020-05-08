6e0ffd6ba40599762689be04fcd1f0fda7fb5cbf
teruya <teru0x01.sheep@gmail.com>
2020-05-08 16:08:39 +0900

    feat: add CreateWorker

--- server.go
+++ server.go
@@ -3,18 +3,22 @@ package server
 import (
 	"net"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 )
 
 type Master struct {
 	listener net.Listener
+	command string
+	commandArgs []string
 	sigCh chan os.Signal
 	workerCh chan WorkerStatus
 }
 
 type WorkerStatus struct {
-	status int
+	exitStatus int
+	err error
 }
 
 func NewMaster(addr string) (*Master, error) {
@@ -32,13 +36,15 @@ func NewMaster(addr string) (*Master, error) {
 func (master *Master) Run() {
 	// create worker and pass socket discriptor, then wait until received signal
 	signal.Notify(master.sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
-
+	
 	for {
+		workerCh := master.CreateWorker()
 		select {
 		case c := <- master.sigCh:
 			//
 			_ = c
-		case c := <- master.workerCh:
+		case c := <- workerCh:
+			// worker exited.
 			_ = c
 		}
 
@@ -46,8 +52,21 @@ func (master *Master) Run() {
 }
 
 // CreateWorker creates listener process and return created process struct.
-func (m *Master) CreateWorker() os.Process {
-	return os.Process{}
+func (master *Master) CreateWorker() chan WorkerStatus {
+	result := make(chan WorkerStatus)
 
-	// 死んだら呼び出し元に通知
+	go func() {
+		cmd := exec.Command(master.command, master.commandArgs...)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		socketFile, err := master.listener.(*net.TCPListener).File()
+		if err != nil {
+			result <- WorkerStatus{err: err,}
+		}
+		cmd.ExtraFiles = append(cmd.ExtraFiles, socketFile)
+		err = cmd.Run()
+		
+	}()
+
+	return result
 }
