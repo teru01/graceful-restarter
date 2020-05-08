1e793b0a0a91c543ccb625cacf377fd3989770ec
teruya <teru0x01.sheep@gmail.com>
2020-05-08 14:55:51 +0900

    feat: update Run

--- cmd/graceful-restarter/main.go
+++ cmd/graceful-restarter/main.go
@@ -1,11 +1,17 @@
+// package main defines command.
+// example: ./graceful-restarter -L 127.0.0.1:8080 [server-command] [args...]
 package main
 
 import (
 	"flag"
 	"fmt"
+
+	"github.com/teru01/graceful-restarter"
 )
 
 func main() {
-	var addr = flag.String("L", "127.0.0.1:0", "listen addr:port")
+	addr := flag.String("L", "127.0.0.1:0", "listen addr:port")
 	fmt.Println(*addr)
+	s, _ := server.NewMaster(*addr)
+	_ = s
 }
--- server.go
+++ server.go
@@ -3,29 +3,51 @@ package server
 import (
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 type Master struct {
 	listener net.Listener
+	sigCh chan os.Signal
+	workerCh chan WorkerStatus
 }
 
-func NewMaster(addr net.Addr) (*Master, error) {
-// create master
-	l, err := net.Listen(addr.Network(), addr.String())
+type WorkerStatus struct {
+	status int
+}
+
+func NewMaster(addr string) (*Master, error) {
+	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
 	return &Master{
 		listener: l,
-	}
+		sigCh: make(chan os.Signal, 1),
+		workerCh: make(chan WorkerStatus, 5), // TODO: define chan size
+	}, nil
 }
 
-func (m *Master) Run() {
-// create worker and pass socket discriptor, then wait until received signal
-	
+func (master *Master) Run() {
+	// create worker and pass socket discriptor, then wait until received signal
+	signal.Notify(master.sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
+
+	for {
+		select {
+		case c := <- master.sigCh:
+			//
+			_ = c
+		case c := <- master.workerCh:
+			_ = c
+		}
+
+	}
 }
 
 // CreateWorker creates listener process and return created process struct.
 func (m *Master) CreateWorker() os.Process {
+	return os.Process{}
 
+	// 死んだら呼び出し元に通知
 }
