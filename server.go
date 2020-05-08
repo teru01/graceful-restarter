43576dc0b6fb13ba88fc5cbce7bb87c31d6ffb0d
teruya <teru0x01.sheep@gmail.com>
2020-05-08 14:25:17 +0900

    feat: add constructor

--- server.go
+++ server.go
@@ -1,3 +1,31 @@
 package server
 
+import (
+	"net"
+	"os"
+)
 
+type Master struct {
+	listener net.Listener
+}
+
+func NewMaster(addr net.Addr) (*Master, error) {
+// create master
+	l, err := net.Listen(addr.Network(), addr.String())
+	if err != nil {
+		return nil, err
+	}
+	return &Master{
+		listener: l,
+	}
+}
+
+func (m *Master) Run() {
+// create worker and pass socket discriptor, then wait until received signal
+	
+}
+
+// CreateWorker creates listener process and return created process struct.
+func (m *Master) CreateWorker() os.Process {
+
+}
