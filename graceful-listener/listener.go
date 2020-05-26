a3ff51835ace40d2235725343fd6399a7379621d
teruya <teru0x01.sheep@gmail.com>
2020-05-26 21:40:05 +0900

    add: gracefulListener

--- graceful-listener/listener.go
+++ graceful-listener/listener.go
@@ -4,18 +4,48 @@ package listener
 import (
 	"net"
 	"os"
+	"sync"
 	"os/signal"
 	"syscall"
 )
 
-// Listen use file descriptor passed by Master as a socket.
-func Listen() (net.Listener, error) {
-	return net.FileListener(os.NewFile(uintptr(3), "dummy"))
+type GracefulListener {
+	listener net.Listener
+	wg sync.WaitGroup
+	quit chan struct{}
 }
 
-func WaitAndGracefulShutdown(l net.Listener) {
+func (l *GracefulListener) WaitShutdownAll() {
+	close(quit)
+	wg.Wait()
+}
+
+// func (l *GracefulListener) Accept() (net.Conn, error) {
+// 	l.wg.Add()
+// }
+
+// handle incomming connection and call server.Handler
+func (l *GracefulListener) serve(server http.Server) {
+	for {
+		conn, err := l.listener.Accept()
+		if err != nil {
+			
+		}
+	}
+}
+
+// Listen use file descriptor passed by Master as a socket.
+func Listen() (net.Listener, error) {
+	return GraceGracefulListener {
+		listener: net.FileListener(os.NewFile(uintptr(3), "dummy")),
+		quit: make(chan struct{})
+	}
+}
+
+func WaitAndGracefulShutdown(l *net.Listener) {
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGTERM)
 	<- ch
-	l.Close() // TODO gracefully
+	l.WaitShutdownAll()
 }
+
